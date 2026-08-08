aa7b98bdb5290bccbb1777c053b21c89e1332bd1
agrikol2 <tim.borodulin17@yandex.ru>
2026-08-08 22:37:57 +0700

    UpdaAdded authors list and Back button to Credits screen - Extended Back button navigation to all other setting buttons

--- ui.py
+++ ui.py
@@ -2,7 +2,7 @@
 # pylint: disable=missing-function-docstring
 
 from tkinter import Frame, Button, Label
-
+from tkinter.ttk import Combobox
 
 def create_menu(root):
 
@@ -32,6 +32,28 @@ def create_menu(root):
         settings_frame = Frame(root, bg="Black")
         settings_frame.pack(padx=200, pady=150)
 
+
+        def back():
+            settings_frame.destroy()
+            menu_frame.pack(padx=200, pady=150)
+            #List of permits in progress
+        resolutions = ["800x600","1920x1080"]
+        resolutions_box = Combobox(settings_frame,values=resolutions,state="readonly")
+        resolutions_box.set("800x600")  
+        resolutions_box.pack(pady=10, ipadx=50, ipady=5)
+
+
+        button_back = Button(
+            settings_frame,
+            text="back",
+            width=5,
+            bg="black",
+            fg="white",
+            relief="flat",
+            command=back,
+        )
+        button_back.pack(pady=10, ipadx=100, ipady=10)
+
     button_settings = Button(
         menu_frame,
         text="Settings",
@@ -44,10 +66,34 @@ def create_menu(root):
     button_settings.pack(pady=10, ipadx=100, ipady=10)
 
     def open_credits():
+        label_credits = Label(
+            root,
+            text="Autors: Saint_Wolf",
+            font=("Times New Roman", 15),
+            bg="Black",
+            fg="White",
+        )
+        label_credits.pack(side="top", anchor="n", padx=10, pady=100)
 
         menu_frame.pack_forget()
         credits_frame = Frame(root, bg="Black")
         credits_frame.pack(padx=200, pady=150)
+
+        def back():
+            credits_frame.destroy()
+            label_credits.destroy()
+            menu_frame.pack(padx=200, pady=150)
+
+        button_back = Button(
+            credits_frame,
+            text="back",
+            width=5,
+            bg="black",
+            fg="white",
+            relief="flat",
+            command=back,
+        )
+        button_back.pack(pady=10, ipadx=100, ipady=55)
 
     button_credits = Button(
         menu_frame,
@@ -75,7 +121,7 @@ def create_menu(root):
 def label(root):
     version_label = Label(
         root,
-        text="Version: 0.0.3",
+        text="Version: 0.0.4",
         font=("Times New Roman", 15),
         bg="black",
         fg="white",
